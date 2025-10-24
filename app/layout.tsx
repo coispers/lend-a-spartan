e5315bd7fc5020595df045d8830687098c757f63
coispers <fmosuela12@gmail.com>
2025-10-24 23:32:33 +0800

    deleted some unnecessary things

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 const _geist = Geist({ subsets: ["latin"] })
@@ -10,7 +9,7 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Lend-A-Spartan",
   description: "Share and borrow items with your BatStateU community",
-  generator: "v0.app",
+  generator: "",
 }
 
 export default function RootLayout({
@@ -22,7 +21,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans antialiased`}>
         {children}
-        <Analytics />
       </body>
     </html>
   )
